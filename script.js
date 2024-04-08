fb9c36611f5756868812565e92f65b50086ea725
Marcus Khalil <148827368+Khalil0713@users.noreply.github.com>
2024-04-09 02:19:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const dropdownToggle = document.querySelector(".dropdown-toggle");
+dropdownToggle.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    const dropdownMenu = document.querySelector("#dropdown > .menu");
+    dropdownMenu.classList.toggle("open");
+    dropdownToggle.classList.toggle("open");
+});
+
